b648709ec5dbfd8f8e1767ac69b05e394b1afbf6
sohaib <sohaib.belaroussi@outlook.com>
2025-07-15 06:36:38 +0800

    last

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.use(express.static("public"));
+app.use(express.static("Public"));
 
 // Helper function to create initial game state
 function createInitialGameState() {
